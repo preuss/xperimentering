6b8b519e33eaae62ab08333402c992e04f82a199
Overwatch <preuss@users.noreply.github.com>
2021-12-11 16:06:53 +0100

    Playing

--- src/main/java/dk/xpreuss/utils/formatter/parser3/Lexer.java
+++ src/main/java/dk/xpreuss/utils/formatter/parser3/Lexer.java
@@ -18,14 +18,14 @@ public class Lexer implements ILexer {
 	@Override
 	public Token readNext() {
 		if (sourceScanner.isEof()) {
-			Token found = new Token(TokenType.END_OF_SOURCE, sourceScanner.getPosition());
+			Token found = new Token(TokenType.END_OF_SOURCE, getPosition());
 		}
 		try (ITokenizer tokenizer = new NewLineTokenizer()) {
 			int needRead = tokenizer.has(sourceScanner.peekCodePoint(tokenizer.needMax()));
 			if (needRead > 0) {
 				List<CodePoint> codePoints = sourceScanner.readCodePoint(needRead);
 				TokenizedToken tokenizedToken = tokenizer.toTokenType(codePoints);
-				int position = sourceScanner.getPosition();
+				int position = getPosition();
 				// Consume.
 				Token token = new Token(
 						tokenizedToken.getTokenType(),
@@ -39,9 +39,9 @@ public class Lexer implements ILexer {
 		try (ITokenizer tokenizer = new SpaceTokenizer()) {
 			StringBuilder spaces = new StringBuilder(1);
 			TokenizedToken tokenizedToken = null;
-			int startPos = sourceScanner.getPosition();
+			int startPos = getPosition();
 			int needRead = 0;
-			while ((needRead = tokenizer.has(sourceScanner.peekCodePoint(tokenizer.needMax())))>0) {
+			while ((needRead = tokenizer.has(sourceScanner.peekCodePoint(tokenizer.needMax()))) > 0) {
 				List<CodePoint> codePoints = sourceScanner.readCodePoint(needRead);
 
 				tokenizedToken = tokenizer.toTokenType(codePoints);
@@ -60,9 +60,9 @@ public class Lexer implements ILexer {
 		try (ITokenizer tokenizer = new FreeTextTokenizer()) {
 			StringBuilder freeText = new StringBuilder(1);
 			TokenizedToken tokenizedToken = null;
-			int startPos = sourceScanner.getPosition();
+			int startPos = getPosition();
 			int needRead = 0;
-			while ((needRead = tokenizer.has(sourceScanner.peekCodePoint(tokenizer.needMax())))>0) {
+			while ((needRead = tokenizer.has(sourceScanner.peekCodePoint(tokenizer.needMax()))) > 0) {
 				List<CodePoint> codePoints = sourceScanner.readCodePoint(needRead);
 				tokenizedToken = tokenizer.toTokenType(codePoints);
 				freeText.append(tokenizedToken.getValue());
@@ -82,7 +82,7 @@ public class Lexer implements ILexer {
 
 		}
 		if (sourceScanner.isEof()) {
-			return new Token(TokenType.END_OF_SOURCE, sourceScanner.getPosition());
+			return new Token(TokenType.END_OF_SOURCE, getPosition());
 		}
 		throw new IllegalStateException();
 	}
